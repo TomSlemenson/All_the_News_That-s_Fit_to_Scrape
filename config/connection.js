dc991474c308a7b2459a885550601af9a62b1a82
Tomas <tslemenson@gmail.com>
2019-07-22 17:34:49 -0700

    config

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+// Connect to the Mongo DB
+require("dotenv").config();
+const mongoose = require("mongoose");
+var MONGODB_URI = process.env.MONGODB_URI|| "mongodb://localhost/mongoHeadlines";
+var connection = mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-require("dotenv").config();
+// require("dotenv").config();
+require("./config/connection.js");
 const express = require("express");
 const path = require('path');
 const logger = require("morgan");
-const mongoose = require("mongoose");
+
+// const mongoose = require("mongoose");
 
 var PORT = process.env.PORT || 3000;
-var MONGODB_URI = process.env.MONGODB_URI|| "mongodb://localhost/mongoHeadlines";
+// var MONGODB_URI = process.env.MONGODB_URI|| "mongodb://localhost/mongoHeadlines";
 
 // Initialize Express
 var app = express();
@@ -32,8 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 require('./router.js')(app);
 
-// Connect to the Mongo DB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// // Connect to the Mongo DB
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Start the server
 app.listen(PORT, function () {
